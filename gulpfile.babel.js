7662235966f2c1dc5b1938c82104ee24fcb63990
littlepad <littlepad@gmail.com>
2016-12-13 23:24:25 +0900

    add sass task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,11 +1,18 @@
 const gulp = require('gulp');
 const jade = require('gulp-jade');
+const sass = require('gulp-sass');
 
 gulp.task('jade', () => {
   gulp.src('./jade/*.jade')
   .pipe(jade({
     'pretty': true
   }))
-  .pipe(gulp.dest('./public/'))
+  .pipe(gulp.dest('./public/'));
+});
+
+gulp.task('sass', () => {
+  return gulp.src('./scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
 });
 
